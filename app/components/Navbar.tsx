5cc933ded31d78f8771df189e603aac4c149e1a6
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-08-12 14:58:48 +0330

    add middleware and next config for support 2 language

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,28 +15,34 @@ const Navbar = () => {
       <Link href="/">
         <Image src={Logo} alt="logo-shirin" className="w-25 h-12" />
       </Link>
-      <ul className="flex justify-evenly w-lg font-bold text-sm text-dark-mode  dark:text-white-bg ">
-        <motion.li
-          whileHover={{ color: '#667d4f' }}
-          className="cursor-pointer  hover:font-bold"
-        >
+      <select
+        name="language"
+        id="language"
+        className="bg-white-500 border-none rounded-lg p-2 outline-none"
+      >
+        <option value="en" className="rounded-lg p-2">
+          English
+        </option>
+        <option value="fa" className="rounded-lg p-2">
+          فارسی
+        </option>
+      </select>
+      <ul className="flex justify-evenly w-sm font-bold text-sm text-dark-mode  dark:text-white-bg ">
+        <motion.li whileHover={{ color: '#667d4f' }} className="cursor-pointer">
           <Link href="/">Home</Link>
         </motion.li>
-        <motion.li
-          whileHover={{ color: '#667d4f' }}
-          className="cursor-pointer hover:font-bold"
-        >
+        <motion.li whileHover={{ color: '#667d4f' }} className="cursor-pointer">
           <Link href="/about-us">About us</Link>
         </motion.li>
         <motion.li
           whileHover={{ color: '#667d4f' }}
-          className="cursor-pointer hover:font-bold"
+          className="cursor-pointer "
         >
           <Link href="/project">Project</Link>
         </motion.li>
         <motion.li
           whileHover={{ color: '#667d4f' }}
-          className="cursor-pointer hover:font-bold"
+          className="cursor-pointer "
         >
           <Link href="/contact">Contact</Link>
         </motion.li>
@@ -57,7 +63,8 @@ const Navbar = () => {
           className="w-10 h-8 transition delay-500 duration-400 ease-in-out cursor-pointer"
         />
       </motion.button>
-      <button className="bg-warning p-2 text-white font-bold rounded cursor-pointer">
+
+      <button className="bg-warning p-2 text-white-bg font-bold rounded cursor-pointer">
         Download Cv
       </button>
     </nav>
