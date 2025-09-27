c9fa062f63744023f038134a8767a59c1a88f64d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-09-27 13:06:17 +0330

    add pathname for link mobile

--- src/app/[locale]/components/Navbar/NavLinks.tsx
+++ src/app/[locale]/components/Navbar/NavLinks.tsx
@@ -1,13 +1,15 @@
 import { useTranslations } from 'next-intl';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const NavLinks = () => {
   const t = useTranslations('navBar');
+  const pathname = usePathname();
   const links = [
     { href: '/', label: t('home') },
-    { href: '#aboutme', label: t('about-me') },
-    { href: '#project', label: t('project') },
-    { href: '#contact', label: t('contact') },
+    { href: `${pathname}#aboutme`, label: t('about-me') },
+    { href: `${pathname}#project`, label: t('project') },
+    { href: `${pathname}#contact`, label: t('contact') },
   ];
   return (
     <ul className="flex justify-evenly w-sm font-bold text-sm items-center text-dark-mode  dark:text-white-bg ">
